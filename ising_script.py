ec6244f259d6b5b5aa8f31c34a9c83b2aaa836e7
Brittany Thompson <72109012+BrittyRenee@users.noreply.github.com>
2021-08-13 02:56:08 -0500

    Update ising_script.py
    Added another looping method to perform multiple experiments at once
    Print an average magnetization that's also stored in the workspace.

--- ising_script.py
+++ ising_script.py
@@ -1,24 +1,30 @@
 # a script that will make a simulation of the ising model
 # based off previous matlab script
 # ultimate practice for python and getting physics experience
-# improvements planned are adding a GUI that will allow manipulation of parameters without going into the script itself
-# another imporvement will finding out how to plot or at least store the data gathered
-# possibly display data and manually mark it down? Or use a different script to continuosly plot data
-
+# improvements planned are adding a slight bias factor in order to get results above and below zero
 import ising_tools
 
 L = 100 #This is the dimension of the LxL matrix
 p = 0.5 # p == % of "spin ups" that are initializing our lattice
-temp = 2 #value for the temperature
+temp = 2.5 #value for the temperature
 #Then the number of trials are determined
-trials = 500
+trials = 500 #number of loops for the Mote Carlo Portion
 
-file_title = input("Please enter name for files: ")
+x = 0 #allocation of our average magnetization
 
-#Then the lattice is set up using the create_lattice function
-initial_lat = ising_tools.create_lattice(L,p)
+file_title = input("Please enter name for image files: ")
 
 # Then the lattice type: 'donut', 'positive', 'negative', or default
 lat_type = 'donut'
 
-final_lat = ising_tools.animate_lattice(trials, initial_lat, L, temp, lat_type, file_title)
+exp_runs = input("Please enter the number of experiments: ")
+exp_runs = int(exp_runs)
+
+for i in list(range(exp_runs)):
+    #Then the lattice is set up using the create_lattice function
+    initial_lat = ising_tools.create_lattice(L,p)
+    final_lat, val = ising_tools.animate_lattice(trials, initial_lat, L, temp, lat_type, file_title)
+    x = x + val
+
+avg_mag = x / exp_runs
+print(avg_mag)
